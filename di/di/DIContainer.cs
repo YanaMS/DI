03da1c40dae93d570da3043bdfa89e7efac9aafd
yana_ms <yanulya.levchuk@gmail.com>
2019-04-01 18:16:00 +0300

    di lesson 1

---
+++ di/di/DIContainer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace DI
+{
+    class DIContainer
+    {
+        private readonly List<object> _objects = new List<object>();
+        public T Register<T>()where T:new()
+        {
+            T instance = new T();
+            return instance;
+        }
+
+        public Worker RegisterWorker()
+        {
+            Worker instance = new Worker();
+            return instance;
+        }
+
+        public Singer RegisterSinger()
+        {
+            Singer instance = new Singer();
+            return instance; 
+        }
+    }
+}
---
+++ di/di/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new DIContainer().Register<Worker>();
+            var b = new DIContainer().Register<Singer>();
+        }
+    }
+}
